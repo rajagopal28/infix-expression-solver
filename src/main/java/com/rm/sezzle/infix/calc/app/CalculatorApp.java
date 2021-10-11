1fcdb8e5a0cab127fa64e2a4d0da98036937907e
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-10-11 10:30:06 +0530

    Initial maven project setup with git ignore

---
+++ src/main/java/com/rm/sezzle/infix/calc/app/CalculatorApp.java
@@ -0,0 +1,8 @@
+package com.rm.sezzle.infix.calc.app;
+
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
---
+++ src/test/java/com/rm/sezzle/infix/calc/app/CalculatorAppTest.java
@@ -0,0 +1,13 @@
+package com.rm.sezzle.infix.calc.app;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorAppTest {
+    @Test
+    public void testMain() {
+        Assert.assertTrue(true);
+        System.out.println("Testing...");
+    }
+}
